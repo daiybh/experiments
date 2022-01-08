047dfe0dc315760283073f19ac12027a827ffbe1
Dai Li <d.li@simplylive.tv>
2022-01-08 22:22:37 +0800

    aa

--- run_experiments.py
+++ run_experiments.py
@@ -1,15 +1,23 @@
 import os
 import subprocess
+import shutil
 
+#"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+def doB(buildType):
+    
+    tempFolder='buildxx\\'+buildType
+	shutil.rmtree(tempFolder)
+    #os.rmdir(tempFolder)
+    os.makedirs(tempFolder, exist_ok=True)    
+    subprocess.check_call('cmake -B {} -G "Ninja" -DCMAKE_BUILD_TYPE:STRING="{}" -DCMAKE_INSTALL_PREFIX=ainstall\\'.format(tempFolder,buildType), shell=True)
+    subprocess.check_call('cmake --build .', shell=True, cwd=tempFolder)
 
-def main():
-    os.makedirs('build', exist_ok=True)
-    subprocess.check_call('cmake -B build -G "NMake Makefiles"', shell=True)
-    subprocess.check_call('cmake --build .', shell=True, cwd='build')
-
-    subprocess.check_call('experiments 1 2 3', shell=True, cwd='build')
-    subprocess.check_call('experiments 2 3 4', shell=True, cwd='build')
+    subprocess.check_call('cmake --install .', shell=True, cwd=tempFolder)
+    
+    #subprocess.check_call('experiments 1 2 3', shell=True, cwd='build')
+    #subprocess.check_call('experiments 2 3 4', shell=True, cwd='build')
 
 
 if __name__ == '__main__':
-    main()
+    doB("Debug")
+    doB("RelWithDebInfo")
